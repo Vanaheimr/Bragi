c9e6a1e9717628931851e94a64648300e74df81e
Achim Friedland <github@ahzf.de>
2012-03-29 13:31:40 +0200

    Import from old Thor project.

---
+++ Bragi/SmallBenchmark/SmallBenchmark.cs
@@ -0,0 +1,199 @@
+﻿/*
+ * Copyright (c) 2010-2011, Achim 'ahzf' Friedland <code@ahzf.de>
+ * This file is part of Balder <http://www.github.com/ahzf/Balder>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using de.ahzf.Blueprints;
+using de.ahzf.Blueprints.PropertyGraphs.InMemory.Mutable;
+using de.ahzf.Blueprints.PropertyGraphs;
+
+#endregion
+
+namespace de.ahzf.Tutorials
+{
+
+    /// <summary>
+    /// A SmallBenchmark
+    /// Do not trust any benchmark you did not manipulate yourself ;)!
+    /// </summary>
+    public class SmallBenchmark
+    {
+
+        #region VertexTypes
+
+        /// <summary>
+        /// The vertex types
+        /// </summary>
+        public enum VertexType
+        {
+            Tag,
+            Website
+        }
+
+        #endregion
+
+        #region Vertex properties
+
+        /// <summary>
+        /// The type property
+        /// </summary>
+        public const String _Type = "Type";
+
+        /// <summary>
+        /// The name property
+        /// </summary>
+        public const String _Name = "Name";
+
+        /// <summary>
+        /// The Url property
+        /// </summary>
+        public const String _Url = "Url";
+
+        #endregion
+
+        #region TagLabel
+
+        /// <summary>
+        /// The tag label
+        /// </summary>
+        public const String _TagLabel = "tag";
+
+        #endregion
+
+
+        #region SmallBenchmark()
+
+        /// <summary>
+        /// A SmallBenchmark
+        /// Do not trust any benchmark you did not manipulate yourself ;)!
+        /// </summary>
+        public SmallBenchmark()
+        {
+
+            var Stopwatch = new Stopwatch();
+            var PRNG      = new Random();
+
+            // Create a new simple property graph
+            var _Graph    = new PropertyGraph();
+
+            var NumberOfUsers           = 20000UL;
+            var NumberOfIterations      = 30;
+            var MinNumberOfEdges        = 15;
+            var MaxNumberOfEdges        = 25;
+
+            var CountedNumberOfEdges    = 0UL;
+
+
+            var d = new Dictionary<UInt64, UInt64>();
+            for (var i = 0UL; i < NumberOfUsers; i++)
+            {
+                d.Add(i, i);
+            }
+
+            Stopwatch.Restart();
+            foreach (var INT in d)
+                CountedNumberOfEdges += INT.Value;
+            Stopwatch.Stop();
+            Console.WriteLine(Stopwatch.Elapsed.TotalMilliseconds + "ms");
+
+            Stopwatch.Restart();
+            var b = new UInt64[NumberOfUsers];
+            foreach (var INT in b)
+                CountedNumberOfEdges += INT;
+            Stopwatch.Stop();
+            Console.WriteLine(Stopwatch.Elapsed.TotalMilliseconds + "ms");
+
+
+            IPropertyVertex<UInt64, Int64, String, String, Object,
+                            UInt64, Int64, String, String, Object,
+                            UInt64, Int64, String, String, Object,
+                            UInt64, Int64, String, String, Object> ActualVertex = null;
+
+            var Vertices = new IPropertyVertex<UInt64, Int64, String, String, Object,
+                                               UInt64, Int64, String, String, Object,
+                                               UInt64, Int64, String, String, Object,
+                                               UInt64, Int64, String, String, Object>[NumberOfUsers + 1];
+
+            var Measurements = new Double[NumberOfIterations];
+            
+            Stopwatch.Start();
+
+            Vertices[0] = _Graph.AddVertex(1-1);
+
+            for (var VertexId = 2UL - 1; VertexId < NumberOfUsers; VertexId++)
+            {
+
+                ActualVertex = _Graph.AddVertex(VertexId, v => v.SetProperty("Name", "User" + VertexId)
+                                                                .SetProperty("Age",  PRNG.Next(0, 150)));
+                Vertices[VertexId-1] = ActualVertex;
+                _Graph.AddEdge(ActualVertex, Vertices[PRNG.Next(0, (Int32) VertexId-1)]);
+
+                var NumberOfAdditionalEdges = (UInt64) PRNG.Next(MinNumberOfEdges, MaxNumberOfEdges);
+
+                do
+                {
+                    _Graph.AddEdge(ActualVertex, Vertices[PRNG.Next(0, (Int32) VertexId - 1)]);
+                } while (ActualVertex.OutDegree() < NumberOfAdditionalEdges);
+
+                if (VertexId % 10000UL == 0)
+                    Console.WriteLine(VertexId);
+
+            }
+
+            Stopwatch.Stop();
+            Console.WriteLine("Creation Time: " + Stopwatch.Elapsed.TotalMilliseconds + "ms");
+            Console.WriteLine();
+
+            //var _ROGraph = new SimpleReadOnlyPropertyGraph(2, _Graph,
+            //                                               SyncedVertexIds:  true,
+            //                                               SyncedEdgeIds:    true
+            //                                               );
+
+            for (var Iteration = 0; Iteration < NumberOfIterations; Iteration++)
+            {
+
+                CountedNumberOfEdges = 0;
+
+                Stopwatch.Restart();
+
+                foreach (var _Vertex in _Graph.Vertices())
+                {
+                    CountedNumberOfEdges += (UInt64) _Vertex.OutDegree();
+                }
+
+                Stopwatch.Stop();
+                Measurements[Iteration] = Stopwatch.Elapsed.TotalMilliseconds;
+                Console.WriteLine(CountedNumberOfEdges + " edges in " + Stopwatch.Elapsed.TotalMilliseconds + "ms");
+
+            }
+
+            var AverageAndStdDev = Measurements.AverageAndStdDev();
+            Console.WriteLine("Mean: " + AverageAndStdDev.Item1 + ", stddev: " + AverageAndStdDev.Item2);
+            Console.ReadLine();
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Bragi/TagExample/TagExample.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Copyright (c) 2010-2011, Achim 'ahzf' Friedland <code@ahzf.de>
+ * This file is part of Balder <http://www.github.com/ahzf/Balder>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using de.ahzf.Balder;
+using de.ahzf.Blueprints.PropertyGraphs;
+using de.ahzf.Blueprints.PropertyGraphs.InMemory.Mutable;
+
+#endregion
+
+namespace de.ahzf.Tutorials
+{
+
+    /// <summary>
+    /// The TagExample
+    /// </summary>
+    public class TagExample
+    {
+
+        #region VertexTypes
+
+        /// <summary>
+        /// The vertex types
+        /// </summary>
+        public enum VertexType
+        {
+            Tag,
+            Website
+        }
+
+        #endregion
+
+        #region Vertex properties
+
+        /// <summary>
+        /// The type property
+        /// </summary>
+        public const String _Type = "Type";
+
+        /// <summary>
+        /// The name property
+        /// </summary>
+        public const String _Name = "Name";
+
+        /// <summary>
+        /// The Url property
+        /// </summary>
+        public const String _Url = "Url";
+
+        #endregion
+
+        #region TagLabel
+
+        /// <summary>
+        /// The tag label
+        /// </summary>
+        public const String _TagLabel = "tag";
+
+        #endregion
+
+
+        #region TagExample()
+
+        public TagExample()
+        {
+
+            // Create a new simple property graph
+            var _graph = new PropertyGraph();
+
+
+            // Add tags
+            var _good  = _graph.AddVertex(v => v.SetProperty(_Type, VertexType.Tag).
+                                                 SetProperty(_Name, "good"));
+
+            var _funny = _graph.AddVertex(v => v.SetProperty(_Type, VertexType.Tag).
+                                                 SetProperty(_Name, "funny"));
+
+            // Add websites
+            var _cnn   = _graph.AddVertex(v => v.SetProperty(_Type, VertexType.Website).
+                                                 SetProperty(_Name, "CNN").
+                                                 SetProperty(_Url,  "http://cnn.com"));
+
+            var _xkcd  = _graph.AddVertex(v => v.SetProperty(_Type, VertexType.Website).
+                                                 SetProperty(_Name, "xkcd").
+                                                 SetProperty(_Url,  "http://xkcd.com"));
+
+            var _onion = _graph.AddVertex(v => v.SetProperty(_Type, VertexType.Website).
+                                                 SetProperty(_Name, "onion").
+                                                 SetProperty(_Url,  "http://theonion.com"));
+
+            // Add edges
+            var _edge1 = _graph.AddEdge(_cnn,   _good,  _TagLabel);
+            var _edge2 = _graph.AddEdge(_xkcd,  _good,  _TagLabel);
+            var _edge3 = _graph.AddEdge(_xkcd,  _funny, _TagLabel);
+            var _edge4 = _graph.AddEdge(_onion, _funny, _TagLabel);
+
+
+
+            // Find out which tags xkcd is tagged with
+            foreach (var _TagName in _xkcd.OutEdges(_TagLabel).InV().Prop(_Name))
+                Console.WriteLine(_TagName);
+
+
+
+            // List tagged sites
+            var _WebList = from   Website
+                           in     _graph.V(v => { return VertexType.Website == v.GetPropertyCasted<String, Object, VertexType>(_Type); })
+                           select new
+                           {
+                               Name  = Website.GetProperty(_Name),
+                               Count = Website.OutEdges(_TagLabel).Count()
+                           };
+
+            foreach (var _Site in _WebList)
+                Console.WriteLine("{0} => {1}", _Site.Name, _Site.Count);
+
+        }
+
+        #endregion
+
+    }
+
+}
