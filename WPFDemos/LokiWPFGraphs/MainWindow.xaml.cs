6d2ff143f93004067e5e8a0ddc6bca5a0597625e
Achim Friedland <github@ahzf.de>
2012-04-11 15:25:35 +0200

    Added Loki WPF Graph visualization demo.

---
+++ WPFDemos/LokiWPFGraphs/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Copyright (c) 2010-2012, Achim 'ahzf' Friedland <code@ahzf.de>
+ * This file is part of Bragi <http://www.github.com/Vanaheimr/Bragi>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Windows;
+
+#endregion
+
+namespace de.ahzf.Bragi.WPFDemos.LokiWPFGraphs
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /// <summary>
+        /// The main window
+        /// </summary>
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            #region Customize the vertex and edge captions
+
+            // Vertices caption
+            GraphCanvas.VertexCaption = v =>
+            {
+                Object Name;
+                if (v.TryGetProperty("Name", out Name))
+                    return Name as String;
+                else
+                    return v.Id.ToString();
+            };
+
+            #endregion
+
+            var Graph = GraphCanvas.Graph;
+
+            var Alice = Graph.AddVertex(v => v.SetProperty("Name", "Alice"));
+            var Bob   = Graph.AddVertex(v => v.SetProperty("Name", "Bob"));
+            var Carol = Graph.AddVertex(v => v.SetProperty("Name", "Carol"));
+            var Dave  = Graph.AddVertex(v => v.SetProperty("Name", "Dave"));
+
+            var e1    = Graph.AddEdge(Alice, "friends", Bob);
+            var e2    = Graph.AddEdge(Bob,   "friends", Carol);
+            var e3    = Graph.AddEdge(Alice, "friends", Carol);
+            var e4    = Graph.AddEdge(Carol, "friends", Dave);
+
+            #region Customize the vertex and edge tooltips
+
+            // Vertices ToolTip
+            GraphCanvas.VertexToolTip = v =>
+            {
+                Object Name;
+                if (v.TryGetProperty("Name", out Name))
+                    return Name as String;
+                else
+                    return v.Id.ToString();
+            };
+
+            // Edges ToolTip
+            GraphCanvas.EdgeToolTip = e => e.Label;
+
+            #endregion
+
+        }
+
+    }
+
+}
